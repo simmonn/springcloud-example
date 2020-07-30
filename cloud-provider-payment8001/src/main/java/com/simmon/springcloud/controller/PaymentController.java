3c420aa80ddb5efd38a0089a314f00e475515d9f
杨胜猛 <yangshengmeng@mininglamp.com>
2020-07-30 17:03:06 +0800

    增加端口提示

--- cloud-provider-payment8001/src/main/java/com/simmon/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/simmon/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.simmon.springcloud.entities.Payment;
 import com.simmon.springcloud.entities.ResponseResult;
 import com.simmon.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -20,17 +21,20 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping("/payment/create")
     public ResponseResult create(@RequestBody Payment payment) {
         int i = paymentService.create(payment);
-        log.info("插入结果:{}", i);
+        log.info("插入结果:{},port:{}", i,serverPort);
         return new ResponseResult(200, "success");
     }
 
     @GetMapping("/payment/{id}")
     public ResponseResult getById(@PathVariable Long id) {
         Payment payment = paymentService.selectById(id);
-        log.info("查询结果:{}", payment);
+        log.info("查询结果:{}.port:{}", payment,serverPort);
         return new ResponseResult(200, "success",payment);
     }
 }
