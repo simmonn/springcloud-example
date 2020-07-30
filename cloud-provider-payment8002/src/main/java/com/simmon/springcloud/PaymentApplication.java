d7a57290b64361a51743c7de3255d925cb29028c
杨胜猛 <yangshengmeng@mininglamp.com>
2020-07-30 16:53:20 +0800

    增加payment 集群8002

---
+++ cloud-provider-payment8002/src/main/java/com/simmon/springcloud/PaymentApplication.java
@@ -0,0 +1,18 @@
+package com.simmon.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author: simmon
+ * @description: TODO
+ * @date: 2020/7/27 18:15
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+}
---
+++ cloud-provider-payment8002/src/main/java/com/simmon/springcloud/service/PaymentService.java
@@ -0,0 +1,16 @@
+package com.simmon.springcloud.service;
+
+
+import com.simmon.springcloud.entities.Payment;
+
+/**
+ * @author: simmon
+ * @description: TODO
+ * @date: 2020/7/28 11:30
+ */
+public interface PaymentService {
+
+    int create(Payment payment);
+
+    Payment selectById(Long id);
+}
