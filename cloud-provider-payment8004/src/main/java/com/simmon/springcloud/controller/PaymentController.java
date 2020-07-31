a1aa3acb2c18bf98b638f71d8b5345700b66dd60
杨胜猛 <yangshengmeng@mininglamp.com>
2020-07-31 15:16:33 +0800

    引入zk注册中心

---
+++ cloud-provider-payment8004/src/main/java/com/simmon/springcloud/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.simmon.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author: simmon
+ * @description: TODO
+ * @date: 2020/7/30 18:01
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/zk")
+    public String paymentZk(){
+        return "spring cloud with zookeeper: " + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
