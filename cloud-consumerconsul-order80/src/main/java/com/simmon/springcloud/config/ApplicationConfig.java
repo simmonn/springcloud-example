53d5b1128341ba27a9b6dba56a771158754e2b58
杨胜猛 <yangshengmeng@mininglamp.com>
2020-07-31 15:19:44 +0800

    增加consul注册中心,增加新模块

---
+++ cloud-consumerconsul-order80/src/main/java/com/simmon/springcloud/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package com.simmon.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author: simmon
+ * @description: TODO
+ * @date: 2020/7/29 18:11
+ */
+@Configuration
+public class  ApplicationConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
---
+++ cloud-providerconsul-payment8006/src/main/java/com/simmon/springcloud/PaymentApplication.java
@@ -0,0 +1,20 @@
+package com.simmon.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: simmon
+ * @description: TODO
+ * @date: 2020/7/30 17:58
+ */
+@SpringBootApplication
+//该注解用于像使用consul或者Zookeeper作为注册中心时注册服务
+@EnableDiscoveryClient
+public class PaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+}
